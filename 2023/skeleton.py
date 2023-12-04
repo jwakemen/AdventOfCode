ede9b69559bd66df643b174c64bd9561b48eefdd
James Wakemen <james_wakemen@cable.comcast.com>
2023-12-04 14:14:28 -0500

    setup initial code skeleton

---
+++ 2023/skeleton.py
@@ -0,0 +1,26 @@
+# imports
+
+# constants
+filename = "input-XX.txt"
+
+# functions
+
+def main():
+    print("starting")
+    # initialize variables
+    total = 0
+
+    with open(filename, "r") as f:
+        # Do the work here
+        for line in f:
+            line = line.strip()
+            value = "TODO"
+            print("line {0:45s} result {1:5}".format(line, value))
+
+    print("done")
+
+    # Print the answer
+    print("total: {0:5d}".format(total))
+
+if __name__ == "__main__":
+    main()
