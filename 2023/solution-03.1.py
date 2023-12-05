12f985c1401dfac2838262e37947c5827fb774b6
James Wakemen <james_wakemen@cable.comcast.com>
2023-12-05 13:32:34 -0500

    2023 day 3

---
+++ 2023/solution-03.1.py
@@ -0,0 +1,53 @@
+# imports
+import re
+
+# constants
+filename = "input-03.txt"
+
+# functions
+def pre_process(line):
+    result = "." + line.strip() + "."
+    return result, len(result)
+
+def process(schematic, line):
+    pattern = re.compile(r"\d+")
+    unwanteds = '.0123456789'
+    part_numbers = []
+    for m in pattern.finditer(schematic[line]):
+        box_top = schematic[line-1][m.start() - 1:m.end() + 1]
+        box_mid = schematic[line][m.start() - 1:m.end() + 1]
+        box_bot = schematic[line+1][m.start() - 1:m.end() + 1]
+
+        if len(box_top.strip(unwanteds)) != 0 or len(box_mid.strip(unwanteds)) != 0 or len(box_bot.strip(unwanteds)) != 0:
+            part_numbers.append(int(m.group(0)))
+
+    return part_numbers
+
+def main():
+    print("starting")
+    # initialize variables
+    total = 0
+    schematic = []
+    
+    with open(filename, "r") as f:
+        # Do the work here
+        max = 0
+        for line in f:
+            pl, ln = pre_process(line)
+            schematic.append(pl)
+            if ln > max:
+                max = ln
+        border = "." * max
+        schematic.insert(0, border)
+        schematic.append(border)
+
+    for i in range(len(schematic)):
+        total += sum(process(schematic, i))
+
+    print("done")
+
+    # Print the answer
+    print("total: {0:5d}".format(total))
+
+if __name__ == "__main__":
+    main()
---
+++ 2023/solution-03.2.py
@@ -0,0 +1,60 @@
+# imports
+import re
+
+# constants
+filename = "input-03.txt"
+
+# functions
+def pre_process(f):
+    schematic = []
+    max = 0
+
+    for line in f:
+        result = "." + line.strip() + "."
+        if len(result) > max:
+            max = len(result)
+        schematic.append(result)
+    
+    border = "." * max
+    schematic.insert(0, border)
+    schematic.append(border)
+    
+    return schematic
+
+def process(schematic):
+    gear_pattern = re.compile(r"\*")
+    nums_pattern = re.compile(r"\d+")
+    ratios = []
+
+    for i in range(len(schematic)):
+        for m in gear_pattern.finditer(schematic[i]):
+            candidates = []
+
+            for x in range(i-1, i+2):
+                for num_match in nums_pattern.finditer(schematic[x]):
+                    if num_match.start() <= m.end() and num_match.end() >= m.start():
+                        candidates.append(num_match.group())
+
+            if len(candidates) == 2:
+                ratios.append(int(candidates[0]) * int(candidates[1]))
+
+    return sum(ratios)
+
+def main():
+    print("starting")
+    # initialize variables
+    schematic = []
+    
+    with open(filename, "r") as f:
+        # Do the work here
+        schematic = pre_process(f)
+
+    result = process(schematic)
+
+    print("done")
+
+    # Print the answer
+    print("result: {0:5d}".format(result))
+
+if __name__ == "__main__":
+    main()
