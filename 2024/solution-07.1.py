d6d5bf99e037de76bc07cdcee92b9a2f212fb293
James Wakemen <james_wakemen@cable.comcast.com>
2024-12-10 10:01:11 -0500

    Day 7

---
+++ 2024/solution-07.1.py
@@ -0,0 +1,102 @@
+# imports
+import logging
+import colorlog
+import argparse
+import sys
+
+# constants
+day = 7
+part = 1
+year = "2024"
+
+# script description
+aoc_name = f"Advent of Code {year:04} - Day {day:02} - Part {part:01}"
+
+# file paths
+infile  = f"inputs/input-{day:02}.txt"
+outfile = f"outputs/output-{day:02}.{part:01}.txt"
+logfile = f"logs/solution-{day:02}.{part:01}.log"
+
+# logger
+logger = logging.getLogger(aoc_name)
+logger.setLevel(logging.DEBUG)
+
+# functions
+def get_args():
+    arg_parser = argparse.ArgumentParser(description=aoc_name, epilog=f"see: https://adventofcode.com/{year:04}/day/{day}")
+    arg_parser.add_argument("-l", "--log", help="set file log level [default: %(default)s]", default="INFO", metavar="LEVEL")
+    arg_parser.add_argument("-c", "--con", help="set console log level [default: %(default)s]", default="WARNING", metavar="LEVEL")
+    return arg_parser.parse_args()
+
+def setup_logger(args):
+    log_level = getattr(logging, args.log.upper(), logging.DEBUG)
+    con_level = getattr(logging, args.con.upper(), logging.WARNING)
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_formatter = colorlog.ColoredFormatter("%(asctime)s %(name)s %(log_color)s%(levelname)-8s%(reset)s %(message)s")
+    stderr_handler.setFormatter(stderr_formatter)
+    stderr_handler.setLevel(con_level)
+    logger.addHandler(stderr_handler)
+
+    logfile_handler = logging.FileHandler(logfile, mode="w")
+    logfile_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)-8s %(message)s")
+    logfile_handler.setFormatter(logfile_formatter)
+    logfile_handler.setLevel(log_level)
+    logger.addHandler(logfile_handler)
+
+    return logger
+
+def parse_line(line):
+    line = line.strip()
+    target, values = line.split(":")
+    target = int(target)
+    values = [int(x) for x in values.strip().split(" ")]
+    return target, values
+
+def possible(target, values):
+    if len(values) < 2:
+        return target == values[0]
+    if values[0] > target:
+        return False
+    a, b, tail = values[0], values[1], values[2:]
+    sum = a + b
+    product = a * b
+    logger.debug(f"sum: {sum} product: {product} tail: {tail}")
+    return possible(target, [sum] + tail) or possible(target, [product] + tail)
+
+# main function
+def main():
+    # setup
+    args = get_args()
+    logger = setup_logger(args)
+    
+    # initialize variables
+    total = 0
+    lines = ""
+
+    logger.info("Starting")
+    
+    # open the input file
+    with open(infile, "r") as f:
+        # Read the input file
+        for line in f:
+            target, values = parse_line(line)
+            logger.info(f"target: {target} values: {values}")
+            p = possible(target, values)
+            if p:
+                total += target
+            logger.info(f"possible: {p}")
+    logger.info(f"input: {lines}")
+
+    # finish up and log the total
+    logger.info("Complete")
+    logger.info(f"Total: {total}")
+
+    # write the output
+    final_output = f"{aoc_name} - total: {total}\n"
+    sys.stdout.write(final_output)
+    with open(outfile, "w") as f:
+        f.write(final_output)
+
+if __name__ == "__main__":
+    main()
